218ec79ee54b798ed355b9223c02ba8a9f9f6721
sumayah194 <saalrowaibaah22@twq.idserve.net>
2022-12-06 10:05:00 +0300

    Initial Commit

---
+++ trry/trryApp.swift
@@ -0,0 +1,17 @@
+//
+//  trryApp.swift
+//  trry
+//
+//  Created by sumayah on 12/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct trryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
